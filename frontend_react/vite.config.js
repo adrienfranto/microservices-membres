917c2bb55bf27ca180de653df8e441d7007c7dc9
adrien franto <radrienfranto2@gmail.com>
2025-08-30 13:35:11 +0300

    Corriger ip en public

--- frontend_react/vite.config.js
+++ frontend_react/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // mandray connection rehetra avy amin'ny réseau
+    port: 5173,       // azonao ovaina raha tianao
+  },
 })
